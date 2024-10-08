e33e594f9050a00940456e61b28384b20fc2459b
Izdadul Aziz <izdadxtyz1@gmail.com>
2024-10-08 09:23:11 +0700

    delete file AppTest.java and create file LexerTest.java

---
+++ src/test/java/com/calculator/LexerTest.java
@@ -0,0 +1,32 @@
+
+
+package com.calculator;
+
+import org.junit.jupiter.api.Test;
+import com.calculator.Lexer;
+import com.calculator.tokens.Token;
+import java.util.ArrayList;
+
+
+public class LexerTest{
+  
+  @Test
+  public void testLexer()  {
+    
+    String code = "1 + 10 * 1 + (10 * 10)";
+    
+    Lexer lexer = new Lexer(code);
+    
+    ArrayList<Token> tokens = lexer.tokenize();
+    
+    System.out.println("code : " + code);
+    for(Token token : tokens){
+      System.out.println("TOKEN ( " + token.getTokenType() + " ) ->" + token.getTokenValue());
+    }
+    
+    
+  }
+  
+}
+
+
